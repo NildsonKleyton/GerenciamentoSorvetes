709c2c9b91f0074b6ba74bc918adcd981c4dbab0
NildsonKleyton <nildsonkleyton@hotmail.com>
2021-04-11 10:54:44 -0300

    Início de implementação de regras

--- src/controle/GerSoveteria.java
+++ src/controle/GerSoveteria.java
@@ -8,30 +8,28 @@ import Entidade.Sorvete;
 
 public class GerSoveteria {
 
-	static List<Sorvete> listaSorvete = new ArrayList<Sorvete>();//cria uma variável global
+	//cria uma variável global
+	static List<Sorvete> listaSorvete = new ArrayList<Sorvete>();
+	static Scanner scan = new Scanner(System.in);
+	static boolean res =false;
+	
 	public static void main(String[] args) {
-		int op = 0, id=-1 ;
+		String op ;
+		int id=-1 ;
 		String sabor = null , tipo = null;
 		double valor;
-		
-		Scanner scan = new Scanner(System.in);
-		Scanner scantx = new Scanner(System.in);
 		do {
-			menu();
-			op = scan.nextInt();
-			
+			op = menu();
+						
 			switch (op) {
-			case 1://inserir
+			case "1"://inserir
 				//recebe valores nos atributos
 				System.out.print("Sabor do Sorvete: ");
-				sabor = scantx.nextLine();
-				
-				System.out.print("\nTipo do Sorvete:\n1 - Fruta\n2 - Essência\nOpção: ");
-				op = scan.nextInt();
-				if(op == 1) {
+				sabor = scan.nextLine();
+				if(op == "1") {
 					tipo ="Fruta";
 				}
-				if(op==2) {
+				if(op=="2") {
 					tipo="Essência";
 				}
 				
@@ -48,43 +46,66 @@ public class GerSoveteria {
 				//add na lista
 				listaSorvete.add(sv);
 				
-				op=1;//ao sair do case retorna ao menu
+				op="1";//ao sair do case retorna ao menu
 				break;
-			case 2://remover 
+			case "2"://remover 
 				lista();
 				System.out.print("Escolha um Sorvete para remover, informando o ID: ");
 				id = scan.nextInt();
 				listaSorvete.remove(id);
-				op=1;//ao sair do case retorna ao menu
+				op="1";//ao sair do case retorna ao menu
 				break;			
-			case 3://listar
+			case "3"://listar
 				lista();
-				System.out.println("\n");
-				op=1;//ao sair do case retorna ao menu
+				op="1";//ao sair do case retorna ao menu
 				break;
-			case 0:
+			case "0":
 				System.out.println("Sistema Finalizado.");
-				op=0;//ao sair do case retorna ao menu
+				op="0";//ao sair do case retorna ao menu
 				break;
 			default:
-				System.out.println("Opção invalida.");
-				op=1;//ao sair do case retorna ao menu
+				System.out.println("\n******Opção invalida.******\n\n");
+				op="1";//ao sair do case retorna ao menu
 				break;
 			}
 	
-		} while (op==1);
+		} while (op=="1");
 	}
-	public static void menu() {
-
-		System.out.println("==============================");
-		System.out.println("        Funcionalidades\n");
-		System.out.println("1 - Inserir sorvete");
-		System.out.println("2 - Remover sorvete");
-		System.out.println("3 - Listar todos os sorvetes");
-		System.out.println("0 - Sair");
-		System.out.println("==============================");
-		System.out.print("Escolha um Opção: ");
+/**
+ * @author Nildson Kleyton (MucuZulo)
+ * @return 
+ * Menu onde retorna a opção escolhida.
+ * <br>1 - Inserir sorvete
+ * <br>2 - Remover sorvete
+ * <br>3 - Listar todos os sorvetes
+ * <br>0 - Sair
+ */
+	public static String menu() {
+		String op ;
+		do {
+			System.out.println("==============================");
+			System.out.println("        Funcionalidades\n");
+			System.out.println("1 - Inserir sorvete");
+			System.out.println("2 - Remover sorvete");
+			System.out.println("3 - Listar todos os sorvetes");
+			System.out.println("0 - Sair");
+			System.out.println("==============================");
+			System.out.print("Escolha um Opção: ");
+			op = scan.nextLine();
+			if (op=="1" ||op=="2" ||op=="3" ||op=="0" ) {
+				res=true;
+			}else {
+				res=false;
+			}
+		} while(res);
+			
+		return op;
 	}
+	
+	/**
+	 * @author Nildson Kleyton(McuZulo)
+	 * @return Retorna uma lista se não estiver vazia.
+	 */
 	public static void lista() {
 		System.out.println("\n");
 		System.out.println("Lista de Sorvetes");
