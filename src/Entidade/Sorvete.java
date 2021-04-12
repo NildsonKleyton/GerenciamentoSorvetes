64665d92d9679f0d1832e20fffc352c664dbd642
Nildson Kleyton <nildsonkleyton@hotmail.com>
2021-04-11 22:10:38 -0300

    add atributos

--- src/Entidade/Sorvete.java
+++ src/Entidade/Sorvete.java
@@ -1,11 +1,12 @@
 package Entidade;
 
 public class Sorvete {
-	private int id;
+	private int cod;
 	private String sabor;
 	private String tipo;// de Fruta ou essência 
+	private double quantidade;
 	private double valor;
-	
+
 	@Override
 	public boolean equals(Object obj) {
 		Sorvete sorveteEntrada = (Sorvete)obj;
@@ -18,34 +19,49 @@ public class Sorvete {
 
 	@Override
 	public String toString() {
-		return "Id="+id+"  sabor=" + sabor + ", tipo=" + tipo + ", valor=" + valor + "";
+		return "Código="+cod+"  sabor=" + sabor + ", tipo=" + tipo + ", valor=" + valor + "";
 	}
 
-	public int getId() {
-		return id;
+	public int getCod() {
+		return cod;
 	}
-	
-	public void setId(int id) {
-		this.id = id;
-	}
+
 	public String getSabor() {
 		return sabor;
 	}
+
 	public String getTipo() {
 		return tipo;
 	}
+
+	public double getQuantidade() {
+		return quantidade;
+	}
+
 	public double getValor() {
 		return valor;
 	}
+
+	public void setCod(int cod) {
+		this.cod = cod;
+	}
+
 	public void setSabor(String sabor) {
 		this.sabor = sabor;
 	}
+
 	public void setTipo(String tipo) {
 		this.tipo = tipo;
 	}
+
+	public void setQuantidade(double quantidade) {
+		this.quantidade = quantidade;
+	}
+
 	public void setValor(double valor) {
 		this.valor = valor;
 	}
 
+	
 	
 }
--- src/controle/GerSoveteria.java
+++ src/controle/GerSoveteria.java
@@ -11,36 +11,45 @@ public class GerSoveteria {
 	//cria uma variável global
 	static List<Sorvete> listaSorvete = new ArrayList<Sorvete>();
 	static Scanner scan = new Scanner(System.in);
-	static boolean res =false;
-	
+	static boolean res=true;
+	static Sorvete sv = new Sorvete();
 	public static void main(String[] args) {
 		String op ;
-		int id=-1 ;
+		int cod=-1 ;
 		String sabor = null , tipo = null;
-		double valor;
+		double valor, qtd;
 		do {
 			op = menu();
 						
 			switch (op) {
 			case "1"://inserir
 				//recebe valores nos atributos
+				//sabor
 				System.out.print("Sabor do Sorvete: ");
 				sabor = scan.nextLine();
+				//tipo
+				System.out.print("\nTipo do Sorvete:\n1 - Fruta\n2 - Essência\nOpção: ");
+				op = scan.nextLine();
 				if(op == "1") {
 					tipo ="Fruta";
 				}
 				if(op=="2") {
 					tipo="Essência";
 				}
-				
-				System.out.print("\nValor do Sorvete Ex.: 1,20 ou 1: R$");
+				//quantidade
+				System.out.println("\nQuantos Litros: ");
+				qtd = scan.nextDouble();
+				//valor
+				System.out.print("\nValor do Sorvete\nEx: 1,20 ou 1:\nR$");
 				valor = scan.nextDouble();
-				id++;
+				cod++;
+				
 				//cria e obj sorvete 
-				Sorvete sv = new Sorvete();
-				sv.setId(id);
+
+				sv.setCod(cod);
 				sv.setSabor(sabor);
 				sv.setTipo(tipo);
+				sv.setQuantidade(qtd);
 				sv.setValor(valor);
 				
 				//add na lista
@@ -50,9 +59,10 @@ public class GerSoveteria {
 				break;
 			case "2"://remover 
 				lista();
-				System.out.print("Escolha um Sorvete para remover, informando o ID: ");
-				id = scan.nextInt();
-				listaSorvete.remove(id);
+				System.out.print("Escolha um Sorvete para remover.\nInformando o ID: ");
+				cod = scan.nextInt();
+				listaSorvete.remove(cod);
+				
 				op="1";//ao sair do case retorna ao menu
 				break;			
 			case "3"://listar
@@ -71,15 +81,15 @@ public class GerSoveteria {
 	
 		} while (op=="1");
 	}
-/**
- * @author Nildson Kleyton (MucuZulo)
- * @return 
- * Menu onde retorna a opção escolhida.
- * <br>1 - Inserir sorvete
- * <br>2 - Remover sorvete
- * <br>3 - Listar todos os sorvetes
- * <br>0 - Sair
- */
+	/**
+	 * @author Nildson Kleyton (MucuZulo)
+	 * @return 
+	 * Menu onde retorna a opção escolhida.
+	 * <br>1 - Inserir sorvete
+	 * <br>2 - Remover sorvete
+	 * <br>3 - Listar todos os sorvetes
+	 * <br>0 - Sair
+	 */
 	public static String menu() {
 		String op ;
 		do {
